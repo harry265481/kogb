5f0a8277d3578d8395ae8cf9da52dea3b37cf0bb
harry265481 <harry.berkley@optusnet.com>
2022-07-20 18:22:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tzyxbw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tzyxbw)
---
+++ pol-office.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'config.php';
+include_once 'functions.php';
+include_once 'header/header.php';
+?>
+<div class="page-header pt-3">
+  <h2>Political HQ</h2>
+</div>
+<?php include_once "footer.php"; ?>
