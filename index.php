da6bbe41a1ebc1e26394bf75e8ca77110d0bee72
Earl_of_Berkeley <harry26548@gmail.com>
2022-07-25 04:15:17 +1000

    modified:   index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link" href="#features">Services</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">History</a></li>
                         <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                         <li class="nav-item"><a class="nav-link" href="sign-in.php">Sign In</a></li>
