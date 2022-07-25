3f00e9c2b4d25ecf3d0fb7f5f5ec23e54fff08eb
Earl_of_Berkeley <harry26548@gmail.com>
2022-07-25 17:19:47 +1000

    modified:   header/header.php 	modified:   header/nav.php     Added font awesome back and replaced some icons

--- header/header.php
+++ header/header.php
@@ -23,6 +23,7 @@ $a = $irlyear / $icyear;
 
     <title>KoGB</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/fb41f04bab.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
 </head>
--- header/nav.php
+++ header/nav.php
@@ -8,16 +8,19 @@ if($_SESSION["adminlevel"] > 0){
     <div id="sidebar" class="show collapse collapse-horizontal">
         <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start min-vh-100">
             <a href="home.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar">
-                <i class="bi bi-house-fill"></i> <span>Home</span>
+                <i class="fa-solid fa-house"></i> <span>Home</span>
             </a>
             <a href="pol-office.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar">
-                <i class="bi bi-house-fill"></i> <span>Political Office</span>
+                <i class="fa-solid fa-building-flag"></i> <span>Political Office</span>
             </a>
             <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar">
-                <i class="bi bi-award-fill"></i></i> <span>Privy Council</span>
+                <i class="fa-solid fa-award"></i> <span>Privy Council</span>
+            </a>
+            <a href="cabinet.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar">
+                <i class="fa-solid fa-users"></i> <span>Cabinet</span>
             </a>
             <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" data-bs-target="#lords-dropdown" data-bs-toggle="collapse">
-                <i class="bi bi-bank2"></i> <span>House of Lords</span>
+            <i class="fa-solid fa-landmark"></i> <span>House of Lords</span>
             </a>
             <div id="lords-dropdown" class="collapse">
                 <div id="lords-nav" class="list-group border-0 rounded-0 text-sm-start">
@@ -26,7 +29,7 @@ if($_SESSION["adminlevel"] > 0){
                     </a>
                 </div>
             </div>
-            <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" data-bs-target="#commons-dropdown" data-bs-toggle="collapse"><i class="bi bi-bank2"></i><span> House of Commons</span></a>
+            <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" data-bs-target="#commons-dropdown" data-bs-toggle="collapse"><i class="fa-solid fa-archway"></i><span> House of Commons</span></a>
             <div id="commons-dropdown" class="collapse">
                 <div id="commons-nav" class="list-group border-0 rounded-0 text-sm-start">
                     <a href="commons.php?house=0" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#commons-dropdown">
@@ -40,7 +43,7 @@ if($_SESSION["adminlevel"] > 0){
                     </a>
                 </div>
             </div>
-            <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" data-bs-target="#icommons-dropdown" data-bs-toggle="collapse"><i class="bi bi-bank2"></i><span> Irish House of Commons</span></a>
+            <a href="#" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" data-bs-target="#icommons-dropdown" data-bs-toggle="collapse"><i class="fa-solid fa-archway"></i><span> Irish House of Commons</span></a>
             <div id="icommons-dropdown" class="collapse">
                 <div id="icommons-nav" class="list-group border-0 rounded-0 text-sm-start">
                     <a href="commons.php?house=1" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#icommons-dropdown">
@@ -54,10 +57,13 @@ if($_SESSION["adminlevel"] > 0){
                     </a>
                 </div>
             </div>
+            <a href="cabinet.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"> 
+                <i class="fa-solid fa-box-archive"></i> <span>Previous Elections</span>
+            </a>
             <?php 
             if($isadmin) {
                 echo'
-                <a href="pending.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate">Pending Characters</a>';
+                <a href="pending.php" class="list-group-item list-group-item-dark border-end-0 d-inline-block text-truncate"><i class="fa-solid fa-clipboard"></i> Pending Characters</a>';
             }
             ?>
         </div>
