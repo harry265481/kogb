48d225db1f871672582ed5cdc6202c3f942b01b7
Earl_of_Berkeley <harry26548@gmail.com>
2022-07-27 09:35:30 +1000

    Session and log in required pages. Removed a test page

--- lords.php
+++ lords.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
 include 'config.php';
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
--- map.php
+++ map.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
 include 'config.php';
 include 'functions.php';
 include 'header/header.php';
